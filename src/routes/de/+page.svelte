feb2219fe65da1319004d1ac1046ce0389f586e8
The-LukeZ <contact.lukez@proton.me>
2026-05-14 01:33:58 +0200

    Fix GitHub link in multiple language pages and remove observability settings from wrangler configuration

--- src/routes/de/+page.svelte
+++ src/routes/de/+page.svelte
@@ -221,7 +221,7 @@
     <b>Kontakt</b>: <a href="mailto:contact@zagpost.org" target="_blank">contact@zagpost.org</a>
   </p>
   <p>
-    <b>GitHub</b>: <a href="https://github.com/org/zagpost" target="_blank">GitHub</a>
+    <b>GitHub</b>: <a href="https://github.com/orgs/zagpost" target="_blank">GitHub</a>
   </p>
   <p>
     <b>Bluesky</b>: <a href="https://bsky.app/profile/zagpost.bsky.social" target="_blank">Bluesky</a>
