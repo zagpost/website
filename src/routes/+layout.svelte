007278310b5d348bd7e80a742941d977abc7f912
The-LukeZ <contact.lukez@proton.me>
2026-05-15 01:16:40 +0200

    Update link href for localized site title in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -45,7 +45,12 @@
     class="fixed top-0 right-0 left-0 mb-8 flex h-(--header-height) w-full items-center justify-between bg-background/30 px-4 py-8 shadow-lg backdrop-blur-lg"
   >
     <div class="mx-auto flex w-full max-w-220 items-center justify-between">
-      <a href="/{data.locale}" class="text-2xl font-bold transition-opacity hover:opacity-70">Zag Post</a>
+      <a
+        href="/{data.locale === 'en' ? '' : data.locale}"
+        class="text-2xl font-bold transition-opacity hover:opacity-70"
+      >
+        Zag Post
+      </a>
       <select class="rounded border p-1 text-sm" onchange={changeLanguage}>
         {#key data.locale}
           <optgroup label={LanguagesLabel[data.locale]}>
