0977d3e199ec1f6dbb6430f4a47d8a3e34424cce
The-LukeZ <contact.lukez@proton.me>
2026-05-15 11:56:51 +0200

    feat: add setHeaders function to manage cache control and content type

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,12 @@
-export async function load({ locals, url }) {
+export async function load({ locals, url, setHeaders }) {
   // Access url to ensure SvelteKit tracks the URL dependency and re-runs the load function on navigation
   url.pathname;
+
+  setHeaders({
+    "cache-control": "public, max-age=86400", // Cache for 1 day
+    "content-type": "text/html; charset=utf-8",
+  });
+
   return {
     locale: locals.locale,
   };
